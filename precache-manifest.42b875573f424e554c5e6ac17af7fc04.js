a1a82c9adeef0ebd808be173ad7e3cdc062d351a
ashiktantony <57289185+ashiktantony@users.noreply.github.com>
2020-06-01 23:49:47 +0530

    Updates

---
+++ precache-manifest.42b875573f424e554c5e6ac17af7fc04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f471a3a9aff594257bcff265afe879d",
+    "url": "/webdemo/index.html"
+  },
+  {
+    "revision": "a3d1f138ffdf9b66d721",
+    "url": "/webdemo/static/css/main.8e25054e.chunk.css"
+  },
+  {
+    "revision": "ed3687f0489def7c3278",
+    "url": "/webdemo/static/js/2.ce310234.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webdemo/static/js/2.ce310234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d1f138ffdf9b66d721",
+    "url": "/webdemo/static/js/main.ed36e35a.chunk.js"
+  },
+  {
+    "revision": "b877a4f0d54f01f742d2",
+    "url": "/webdemo/static/js/runtime-main.f27330e3.js"
+  }
+]);
